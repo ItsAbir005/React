ef1f1d262cd4c638204ffd719f080d2ff0e9e939
Abir Maity <abirmaity44@gmail.com>
2025-11-12 00:56:46 +0530

    config redux toolkit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,34 @@
-import React from "react";  
+import React, { useState, useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 import './App.css';
+import authService from "./appwrite/auth"
+import { login, logout } from "./store/authSlice"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Welcome to My React App</h1>
-        <p>This is a simple React application.</p>
-      </header>
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch()
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }))
+        } else {
+          dispatch(logout())
+        }
+      })
+      .finally(() => setLoading(false))
+  }, [])
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+          TODO:  <Outlet />
+        </main>
+        <Footer />
+      </div>
     </div>
-  );
+  ) : null
 }
 
 export default App;
